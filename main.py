78b638d2ab44f707b8106579a726ff2c2eb86d33
TinyZorro <admin@tinyfox.dev>
2022-06-01 22:12:17 -0600

    Add Image album request command

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from telegram.ext import Defaults, Updater, CommandHandler, Filters, CallbackCon
 from telegram.utils.request import Request
 from telegram import Update
 import threading
+import requests
 import telegram
 import json
 import io
@@ -39,6 +40,18 @@ def start(update: Update, context: CallbackContext):
     return update.message.reply_text('Hello World!')
 
 
+def image(update: Update, context: CallbackContext):
+    command = update.message.entities[0]
+    command = update.message.text[command['offset']+1:command['length']]
+    album = []
+    if context.args and context.args[0].isnumeric() and 11 > int(context.args[0]) > 1:
+        for i in range(int(context.args[0])):
+            album.append(telegram.InputMediaPhoto("https://api.tinyfox.dev" + requests.get(f'https://api.tinyfox.dev/img?animal={command}&json').json()['loc']))
+    else:
+        album.append(telegram.InputMediaPhoto("https://api.tinyfox.dev" + requests.get(f'https://api.tinyfox.dev/img?animal={command}&json').json()['loc']))
+    update.message.reply_media_group(album)
+
+
 if __name__ == '__main__':
     conf = Config('conf.json')
     bot = telegram.ext.ExtBot(conf.token, request=Request(con_pool_size=70, connect_timeout=120),
@@ -48,5 +61,7 @@ if __name__ == '__main__':
     updater = Updater(bot=bot, workers=60, use_context=True)
     dispatch = updater.dispatcher
     dispatch.add_handler(CommandHandler('start', start, Filters.chat_type.private), group=0)
+    available_animals = requests.get('https://api.tinyfox.dev/img').json()['available']
+    dispatch.add_handler(CommandHandler(available_animals, image), group=0)
     print(f'Started {bot.username} with settings: {conf}')
     updater.start_polling()
